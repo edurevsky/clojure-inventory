b65d183a897cddc12fb12b7cae10e20904cbef28
edurevsky <edurevsky@gmail.com>
2022-08-29 12:03:21 -0300

    add /items route

--- src/inventory/routes.clj
+++ src/inventory/routes.clj
@@ -1,10 +1,19 @@
 (ns inventory.routes
   (:require [inventory.handlers :as handlers]
-            [inventory.middlewares :as middlewares]))
+            [inventory.middlewares :as middlewares]
+            [schema.core :as s]
+            [inventory.models :as models]))
 
 (def routes
   [["/ok" {:name ::ok
            :get  {:handler (fn [_] {:status 200})}}]
    ["/inventory" {:name ::inventory
                   :get  {:middleware [[middlewares/db-middleware]]
-                         :handler handlers/handle-inventory-info}}]])
+                         :responses  {200 {:body {:inventory-items s/Int
+                                                  :inventory-value s/Num}}}
+                         :handler    handlers/handle-inventory-info}}]
+   ["/items" {:name ::items
+              :post {:middleware [[middlewares/db-middleware]]
+                     :parameters {:body models/NewItem}
+                     :responses  {201 {:body models/Item}}
+                     :handler    handlers/handle-add-item}}]])
