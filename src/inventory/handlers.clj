b07eb773f630611b19a3f23cefb7ad8aa07adf49
edurevsky <edurevsky@gmail.com>
2022-08-29 12:03:11 -0300

    add handle-add-item

--- src/inventory/handlers.clj
+++ src/inventory/handlers.clj
@@ -4,4 +4,11 @@
 (defn handle-inventory-info
   [{:keys [db]}]
   {:status 200
-   :body (logic/inventory-info @db)})
+   :body   (logic/inventory-info @db)})
+
+(defn handle-add-item
+  [{:keys [db body-params]}]
+  (let [item (assoc body-params :id (random-uuid))]
+    (swap! db logic/add-item item)
+    {:status 201
+     :body   (get @db (:id item))}))
