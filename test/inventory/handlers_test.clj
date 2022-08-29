191ee96491ca67c4161273b45743fe3dde805cd4
edurevsky <edurevsky@gmail.com>
2022-08-29 13:47:42 -0300

    add handler tests

---
+++ test/inventory/handlers_test.clj
@@ -0,0 +1,22 @@
+(ns inventory.handlers-test
+  (:require [clojure.test :refer :all]
+            [inventory.handlers :refer :all]
+            [schema.core :as s]
+            [inventory.models :as m]))
+
+(deftest handle-get-item-by-id-test
+  (testing "Should return map with status 404."
+    (let [mocked-request {:db          (atom {})
+                          :path-params {:id "id"}}
+          expected-status 404]
+      (is (:status (handle-get-item-by-id mocked-request) expected-status))))
+  (testing "Should return map with status 200 and body that is valid as an Item."
+    (let [gen-id (random-uuid)
+          mocked-request {:db          (atom {gen-id {:id gen-id :name "name" :price 0.1 :quantity 1}})
+                          :path-params {:id (str gen-id)}}
+          resp (handle-get-item-by-id mocked-request)
+          expected-status 200]
+      (is
+        (and
+          (= (:status resp) expected-status)
+          (s/validate m/Item (:body resp)))))))
