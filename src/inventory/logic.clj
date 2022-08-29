8a2ab1a315086cc00e47b4edf76953938c63d256
edurevsky <edurevsky@gmail.com>
2022-08-29 16:36:53 -0300

    add item-purchase logic

--- src/inventory/logic.clj
+++ src/inventory/logic.clj
@@ -1,18 +1,22 @@
 (ns inventory.logic)
 
 (defn add-item
+  "Adds the item in the inventory map."
   [inventory item]
   (assoc inventory (:id item) item))
 
 (defn remove-item
+  "Removes the item of the inventory map."
   [inventory item-id]
   (dissoc inventory item-id))
 
 (defn item-total
+  "Returns the item price times the item quantity."
   [item]
   (* (:price item) (:quantity item)))
 
 (defn item-with-total-price
+  "Returns the item with the total price."
   [inventory item-id]
   (let [item (get inventory item-id)
         total (item-total item)]
@@ -20,9 +24,26 @@
         (assoc :total-price total))))
 
 (defn inventory-info
+  "Returns a map containing the quantity of the inventory items
+   and the total value of the inventory."
   [inventory]
   (let [each-item (vals inventory)
         each-total (map item-total each-item)
         total (reduce + each-total)]
     {:inventory-items (count inventory)
      :inventory-value total}))
+
+(defn has-stock?
+  [item-quantity purchase-amount]
+  (< item-quantity purchase-amount))
+
+(defn item-purchase
+  "Returns the updated inventory with the updated Item."
+  [inventory purchase]
+  (let [id (:item/id purchase)
+        item (get inventory id)
+        purchase-amount (:amount purchase)
+        item-quantity (:quantity item)]
+    (if (has-stock? item-quantity purchase-amount)
+      (throw (ex-info "Purchase amount is higher than the item quantity." {:type ::purchase-ex}))
+      (update-in inventory [id :quantity] - purchase-amount))))
