fde911ec76d201759703d616071f06054f4517ef
edurevsky <edurevsky@gmail.com>
2022-08-29 14:00:13 -0300

    add total-price at item schema

--- src/inventory/models.clj
+++ src/inventory/models.clj
@@ -2,10 +2,11 @@
   (:require [schema.core :as s]))
 
 (def Item
-  {:id       s/Uuid
-   :name     s/Str
-   :quantity s/Int
-   :price    s/Num})
+  {:id          s/Uuid
+   :name        s/Str
+   :quantity    s/Int
+   :price       s/Num
+   :total-price s/Num})
 
 (def NewItem
   {:name     s/Str
