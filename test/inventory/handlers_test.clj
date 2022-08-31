a4c641e3368ac7f2afbeca1fe2a5f6ad44d1decc
edurevsky <edurevsky@gmail.com>
2022-08-31 14:59:57 -0300

    item purchase tests

--- test/inventory/handlers_test.clj
+++ test/inventory/handlers_test.clj
@@ -35,3 +35,43 @@
           resp (handle-delete-item-by-id mocked-request)
           expected-status 204]
       (is (= (:status resp) expected-status)))))
+
+(deftest handle-item-purchase-test
+  (let [id #uuid"b894ffbc-f586-47a2-a7ee-77ad775d96b4"
+        item {:id id :name "?" :price 0.01 :quantity 2321}
+        initial-value {id item}]
+    (testing "That returns a map with status 400 when requesting with an non existing id."
+      (let [mocked-request {:db          (atom initial-value)
+                            :body-params {:item-id (str (random-uuid)) :amount 1}}
+            resp (handle-item-purchase mocked-request)]
+        (is (= 400
+               (:status resp)))))
+    (testing "That returns a map with status 400 when requesting an amount that is higher than the available."
+      (let [mocked-request {:db          (atom initial-value)
+                            :body-params {:item-id (str id) :amount 2322}}
+            resp (handle-item-purchase mocked-request)]
+        (is (= 400
+               (:status resp)))))
+    (testing "That returns a map with status 204 when requesting an amount that is the same as the available."
+      (let [mocked-request {:db          (atom initial-value)
+                            :body-params {:item-id (str id) :amount 2321}}
+            resp (handle-item-purchase mocked-request)]
+        (is (= 204
+               (:status resp)))))
+    (testing "That returns a map with status 204 when requesting an amount that is below the available."
+      (let [mocked-request {:db          (atom initial-value)
+                            :body-params {:item-id (str id) :amount 2320}}
+            resp (handle-item-purchase mocked-request)]
+        (is (= 204
+               (:status resp))))))
+  (testing "That returns a map with status 204."
+    (let [id #uuid"fdc8d2eb-ea52-4cf0-81c2-b5392afe822f"
+          item {:id       id
+                :name     "xpto"
+                :price    0.99
+                :quantity 10}
+          mocked-request {:db          (atom {id item})
+                          :body-params {:item-id "fdc8d2eb-ea52-4cf0-81c2-b5392afe822f" :amount 1}}
+          resp (handle-item-purchase mocked-request)]
+      (is (= 204
+             (:status resp))))))
